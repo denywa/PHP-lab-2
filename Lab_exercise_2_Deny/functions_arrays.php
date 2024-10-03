b0b3f357107b5fcbc010e0c40a1b423b15dcf568
Deny W <115266792+denywa@users.noreply.github.com>
2024-10-03 12:54:51 +0800

    Add files via upload

--- Lab_exercise_2_Deny/functions_arrays.php
+++ Lab_exercise_2_Deny/functions_arrays.php
@@ -3,14 +3,20 @@ ob_start();
 include 'variables.php';
 ob_end_clean(); //untuk kasi hilang output dari file yang di include
 function greet($student){
-    $msg = "Hello ". $student.'!';
+    $msg = 'Hello '. $student.'!';
     return $msg;
 }
 
 $students = array($student1->name, $student2->name, $student3->name, $student4->name, $student5->name);
 
+// Looping array menggunakan foreach
 foreach ($students as $student){
     echo greet($student).'<br>';
+}
+
+// Looping array menggunakan normal for
+for ($i = 0; $i < count($students); $i++){
+    echo greet($students[$i]).'<br>';
 }
 
 ?>
